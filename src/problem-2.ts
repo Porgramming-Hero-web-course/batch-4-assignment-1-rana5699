b75f6da5dbe4945fe96b1c5d661b3088d78831cb
unknown <ranaot56@gmail.com>
2024-11-06 23:01:10 +0600

    problem number 2 sloved

---
+++ src/problem-2.ts
@@ -0,0 +1,8 @@
+const removeDuplicates = (input: number[]): number[] => {
+  const uniqueNumbers = input.filter(
+    (num, index) => input.indexOf(num) === index
+  );
+  return uniqueNumbers;
+};
+
+// console.log(removeDuplicates([1, 2, 2, 3, 88, 88, 4, 4, 5, 9, 9]))
