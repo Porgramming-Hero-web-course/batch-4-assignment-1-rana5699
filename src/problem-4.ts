9f80875982826c928ef6765b488eac33c520f675
unknown <ranaot56@gmail.com>
2024-11-06 23:48:08 +0600

    slove problem number 4

---
+++ src/problem-4.ts
@@ -0,0 +1,27 @@
+interface Circle {
+  shape: "circle";
+  radius: number;
+}
+
+interface Rectangle {
+  shape: "rectangle";
+  width: number;
+  height: number;
+}
+
+type Shape = Circle | Rectangle;
+
+const calculateShapeArea = (shape: Shape) => {
+  if (shape.shape === "circle") {
+    const result = Math.PI * shape.radius * shape.radius;
+    return result;
+  } else {
+    const result = shape.width * shape.height;
+    return result;
+  }
+};
+// const rectangleArea = calculateShapeArea({
+//   shape: "rectangle",
+//   width: 4,
+//   height: 6,
+// });
