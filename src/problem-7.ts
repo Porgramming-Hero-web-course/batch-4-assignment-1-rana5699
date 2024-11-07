cecb7a376dc8bdee997fa358038f1275e7a5008c
unknown <ranaot56@gmail.com>
2024-11-07 15:21:53 +0600

    slove problem number 7

---
+++ src/problem-7.ts
@@ -0,0 +1,24 @@
+class Car {
+  make: string;
+  model: string;
+  year: number;
+
+  constructor(make: string, model: string, year: number) {
+    this.make = make;
+    this.model = model;
+    this.year = year;
+  }
+
+  //   getCarAge method
+  getCarAge(): number {
+    const currentYear: number = new Date().getFullYear();
+
+    const carAge = currentYear - this.year;
+    console.log("Car  carAge:", carAge);
+
+    return carAge;
+  }
+}
+
+const car = new Car("Honda", "Civic", 2019);
+console.log(car.getCarAge());
